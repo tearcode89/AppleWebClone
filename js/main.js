31795e87932889dd2b63fb29e561450c48a4c3a5
tearcode89 <sjun8932.3@gmail.com>
2023-08-02 18:03:15 +0900

    10.현재 활성시킬 씬 결정하기

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 (() => {
-    // sceneInfo는 각 scene에 대한 정보를 담고 있다.
-    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
 
+    let yOffset = 0; // window.pageYOffset 대신에 쓸 변수
+    let PrevScrollHeight = 0; // 현재 스크롤 위치(yOffset) 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된 (눈 앞에 보고있는) 씬 (Scrollsection)
+    // sceneInfo는 각 scene에 대한 정보를 담고 있다.
     const sceneInfo = [
         {
             // 0
@@ -49,15 +51,29 @@
         }
     }
 
-    function ScrollLoop() {
+    function scrollLoop() {
+        PrevScrollHeight = 0; // PrevScrollHeight의 초기화가 함수 내에 있어야 맥북 기준 4 scene의 총합 11540이 찍히고 다음 scrollLoop() 이 실행될 때 다시 초기화 된다. (값이 누적되지 않는다.) 
+        for (let i = 0; i < currentScene; i++) {
+            PrevScrollHeight += sceneInfo[i].scrollHeight;
+        }
 
+        if(yOffset > PrevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if(yOffset < PrevScrollHeight) {
+            if (currentScene === 0) return; // scene 0에서 바운싱 스크롤이 일어났을 때 콘솔에 -값을 찍히지 않게 방지
+            currentScene--;
+        }
+
+        console.log(currentScene);
     }
 
     window.addEventListener('resize', setLayout) // 화면 크기가 바뀌었을때를 기준으로 setLayout을 정의하기
     window.addEventListener('scroll', () => {
-        yOffset = window.scrollY;
-        ScrollLoop()
-    })
+        yOffset = window.scrollY;  // window.pageYOffset은 deprecated 되었다. 
+        scrollLoop()
+    });
 
     setLayout()
 
