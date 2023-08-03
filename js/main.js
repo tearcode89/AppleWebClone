6bb32fb4ebabbc8adc2f0c33301ee9c38e03b2ba
tearcode89 <sjun8932.3@gmail.com>
2023-08-03 14:05:09 +0900

    16.특정 타이밍 스크롤 애니메이션 기능 추가

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d')
             },
             values: {
-                messageA_opacity: [0, 1]
+                messageA_opacity: [0, 1, { start: 0.1, end: 0.2}],
+                messageB_opacity: [0, 1, { start: 0.3, end: 0.4}]
             }
         },
         {   
@@ -74,9 +75,25 @@
     function calcValues (values, currentYOffset) { // currentYOffset은 현재 씬에서 얼마나 스크롤 됐는지를 의미
         let rv;
         // 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
-        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight
+        const scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
 
-        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        if (values.length === 3) {
+            // start ~ end 사이의 애니메이션을 실행
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart
+
+            if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+                rv = (currentYOffset - partScrollStart) / partScrollStart * (values[1] - values[0]) + values[0];
+            } else if (currentYOffset < partScrollStart) {
+                rv = values[0];
+            } else if (currentYOffset > partScrollEnd) {
+                rv = values[1];
+            }
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
 
         return rv;
     }
